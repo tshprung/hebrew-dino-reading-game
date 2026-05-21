954af25e7b6bf2a6cdcff2a050c87aa8622f93f1
tshprung <tshprung@gmail.com>
2026-05-21 13:02:57 +0200

    Android 16 prep: edge-to-edge + appCategory game

--- app/src/main/java/com/tal/hebrewdino/MainActivity.kt
+++ app/src/main/java/com/tal/hebrewdino/MainActivity.kt
@@ -7,18 +7,33 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.tal.hebrewdino.ui.AppNav
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    AppNav()
+                    Box(
+                        modifier =
+                            Modifier
+                                .fillMaxSize()
+                                .windowInsetsPadding(WindowInsets.safeDrawing),
+                    ) {
+                        AppNav()
+                    }
                 }
             }
         }
